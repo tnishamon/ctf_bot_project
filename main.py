eb13264e694610db6b0870d7ec6df75f2081bd62
Matthew Noojin <noojin@unlv.nevada.edu>
2025-09-20 14:15:26 -0700

    Commiting basic logic, using discord py for now

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord 
+
+class Client(discord.Client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return 
+        if message.content.startswith('Dylan'):
+            await message.channel.send('I hate Dylan')
+        if message.content.startswith('Matt'):
+            await message.channel.send('Oh man, I love Matt so much and hate Dylan')
+        
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents = intents)
+client.run('')
