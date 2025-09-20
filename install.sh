9dd257b7adee4a8ddf36d5e774cac2b07e38e4c3
Matthew Noojin <noojin@unlv.nevada.edu>
2025-09-20 15:01:54 -0700

    Added python venv support

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-python3 -m pip install discord.py
+sudo apt-get install python3-venv
 
-python3 -m pip install python-dotenv
+python -m venv myenv 
+
+source myenv/bin/active
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
