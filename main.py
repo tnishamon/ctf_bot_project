81e54d4c5f79539952801b3e1e11cedd2ac0bd10
Matthew Noojin <noojin@unlv.nevada.edu>
2025-09-27 14:06:20 -0700

    Added new packages for api requests and command functionality

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import discord 
+from discord.ext import commands
 from os import environ
+import os
 from dotenv import load_dotenv
 import sqlite3 
+import requests 
 
 load_dotenv()
 # Token from .env
 token=environ["DISCORD_BOT_TOKEN"]
 
+
 # Class for discord events, will need to refactor later
 class Client(discord.Client):
-    # Track different messages input
-    async def on_message(self, message):
-        if message.author == self.user:
-            return 
-        if message.content.startswith('Dylan'):
-            await message.channel.send('I hate Dylan')
-        if message.content.startswith('Matt'):
-            await message.channel.send('Oh man, I love Matt so much and hate Dylan')
-
+    async def on_ready(self):
+        print('Testing print :)')
+        
+        try:
+            guild = discord.Object(id=1391665916194066483)
+            synced = await self.tree.sync(guild=guild)
+        except Exception as e:
+            print('Error')
+        
 # Declares our discord intent, which Discord needs specified. 
 intents = discord.Intents.default()
 intents.message_content = True
 
+client = commands.Bot(command_prefix='!', intents=intents)
+
+GUILD_ID = discord.Object(id=1391665916194066483)
+
+@client.command(guild = GUILD_ID)
+async def test(ctx):
+    response = requests.get('https://ctftime.org/api/v1/top/')
+    data = response.json()
+    await ctx.channel.send(data)
+
 # Run client locally 
-client = Client(intents = intents)
 client.run(token)
